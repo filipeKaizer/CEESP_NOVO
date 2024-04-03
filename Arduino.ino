1c7198b7b532f98ee1c6b240507836bc226698cc
filipeKaizer <98619997+filipeKaizer@users.noreply.github.com>
2024-04-03 14:45:48 -0300

    Update Arduino.ino
    Melhorias no desempenho;
    Correção do registro de RPM e dados do MWW02;
    Comentários melhorados;
    Sistema de sebug ajustado (função única).

--- Arduino.ino
+++ Arduino.ino
@@ -1,24 +1,21 @@
-////////////////////////////////   Modbus Master   ////////////////////////////////
 #include <ModbusMaster.h>
+
+//----------------------------------- DEFINIÇÕES -----------------------------------//
 ModbusMaster node;          // instantiate ModbusMaster object
 
-#define MAX485_DE      3    // Pino de sentido de transmissão
-#define ledRed         A0   // led - Red
-#define ledGreen       A1   // led - Green
-#define ledBlue        A2   // led - Blue
-#define sensorRPM      2    // Sensor de RPM, HIGH quando interrompido
-#define sensorSignal   10   // Sensor de RPM paralelo ao 2
+#define MAX485_DE      3      // Pino de sentido de transmissão
+#define ledRed         A0     // led - Red
+#define ledGreen       A1     // led - Green
+#define ledBlue        A2     // led - Blue
+#define sensorRPM      2      // Sensor de RPM, HIGH quando interrompido
 
-#define MMW2_ID        1    // ID do dispositivo medidor MMW02
-#define AddrNum        82   // Numero de endereços obtidos pelo MMW02
-#define refresh        60   // Tempo entre atualizações de dados do arduino (60 -> 60ms)
-#define tempoLimite    200  // Tempo máximo de espera do envio de dados pelo MW02 (100 -> 0,1seg)
+#define MMW2_ID        1      // ID do dispositivo medidor MMW02
+#define AddrNum        70     // Numero de endereços obtidos pelo MMW02
+#define refresh        60     // Tempo entre atualizações de dados do arduino (60 -> 60ms)
 #define refreshConnection 500 // Tempo de espera para o software enviar valores de teste
+//--------------------------------------------------------------------------------//
 
-
-//---------------------------------------------------------------------------------
-///////////////////////////////   Variaveis   /////////////////////////////////////
-bool ConEst = false;        // Flag que controla o envio de dados para o computador. Se não houver um, o mesmo não envia
+//----------------------------------- VARIAVEIS ----------------------------------//
 bool Debug = false;         // Flag que habilita a função debug. 
 
 // Variáveis para o cálculo das RPM
@@ -34,40 +31,39 @@ struct Valor {
 
 //// Especifica um vetor da struct valor
 Valor valores[AddrNum];
+//--------------------------------------------------------------------------------//
 
-
-///////////////////////// COMANDOS //////////////////////////////////// 
+//------------------------------------ COMANDOS ----------------------------------//
 /*
   snd      -> envio de dados concatenados 
   serial   -> Envio de dados como lista
   test     -> Função de compatibilidade
   debug    -> Ativa a função de debug, mostrando leitura em tempo real
 */
+//--------------------------------------------------------------------------------//
 
-//////////////////////////////// SETUP ////////////////////////////////
+//------------------------------------- SETUP ------------------------------------//
 void setup(){
-  pinMode(MAX485_DE, OUTPUT);                   // Pino de controle do MAX485
   pinMode(ledRed, OUTPUT);                      // LED - Red
   pinMode(ledGreen, OUTPUT);                    // LED - Green
   pinMode(ledBlue, OUTPUT);                     // LED - Blue
 
-  pinMode(sensorRPM, INPUT);                // RPM 
+  pinMode(sensorRPM, INPUT);                    // RPM 
   attachInterrupt(digitalPinToInterrupt(sensorRPM), countPulse, FALLING); // Ativa a interrupção no pino do sensor
-  
-  digitalWrite(MAX485_DE, 0);                   // Sentido do MAX485     
    
-  Serial.begin(9600);                           // Monitor serial para visualização de dados.
+  pinMode(MAX485_DE, OUTPUT);                   // Pino de controle do MAX485
+  digitalWrite(MAX485_DE, 0);                   // Sentido do MAX485     
   Serial1.begin(9600, SERIAL_8N1);              // Serial para comunicação com o MMW02
+  Serial.begin(9600);                           // Monitor serial para visualização de dados.
   
   node.begin(MMW2_ID, Serial1);                 // Inicia o objeto node para comunicação com o MMW02
-  node.preTransmission(preTransmission);
-  node.postTransmission(postTransmission);
+  node.preTransmission(preTransmission);        // Modbus
+  node.postTransmission(postTransmission);      // Modbus
 }
-// ----------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------//
 
-//////////////////////////////// LOOP ///////////////////////////////////////////////
-void loop()
-{
+//-------------------------------------- LOOP -------------------------------------//
+void loop() {
   int req = softwareReq(); //Verifica requisição do software
   
   switch(req){
@@ -87,20 +83,42 @@ void loop()
      ledState('l');
      Leitura();
      readRPM();
+     if (Debug)
+       debug();
     break;    
   }
-
   delay(refresh);
 }
-// ------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------//
 
-//////////////////////// LEITURA/ESCRITA //////////////////////////////////////////////
-void Escrita(uint32_t address, uint8_t value) {
-    preTransmission();
-    node.writeSingleRegister(address,value);
-    postTransmission();
+//----------------------------------- DEBUG MODE -----------------------------------//
+void debug() {
+    // Dados coletados do modbus 
+    Serial.print("\n\tMULTIMEDIDOR\t\n");
+    if (valores[0].valor != 0) {
+      Serial.print("Erro: \t");
+      Serial.print(valores[0].valor);
+      Serial.print("\n");
+    } else {
+      mostraSerial();
+    }
+
+    // RPM
+    Serial.print("\n\tRPM\t\n");
+    if (valores[44].valor < 10) {
+      Serial.print("Erro: \t");
+      Serial.print("Motor parado ou lento (");
+      Serial.print(valores[44].valor);
+      Serial.print("RPM)\n");
+    } else {
+      Serial.print("RPM: \t");
+      Serial.print(valores[44].valor);
+      Serial.print("RPM\n");
+    }
 }
+//---------------------------------------------------------------------------------//
 
+//------------------------------------ LEITURA ------------------------------------//
 void Leitura(){
   uint16_t result, result2;
   float floatValue;
@@ -114,9 +132,8 @@ void Leitura(){
     // Adiciona o codigo de erro 0 (success)
     valores[0].add = 0;
     valores[0].valor = 0;
-    
+
     for (int address = 0, pos = 1; address < AddrNum; address += 2, pos++) {
- 
         // Obtem os dados em buffer
         result = node.getResponseBuffer(address);
         result2 = node.getResponseBuffer(address + 1);
@@ -132,72 +149,44 @@ void Leitura(){
         // Adiciona os valores no registro
         valores[pos].add = address+2;
         valores[pos].valor = res;
-
-        //Função Debug
-        if (Debug) {
-          ledState('d');
-          Serial.print("Valor: ");
-          Serial.println(res);
-        }
     }
   } else {
-    if (Debug) {
-      ledState('d');
-      Serial.print("Erro: ");
-      Serial.println(readStatus);
-    }
-
     valores[0].add = 0;
     valores[0].valor = readStatus;
+    
     ledState('e');
   }
 }
-//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------//
 
-//////////////////////////////// RPM ///////////////////////////////////////////////////
-// Função de interrupção para contar os pulsos
-void countPulse() {
+//-------------------------------------- RPM --------------------------------------//
+void countPulse() { // Função de interrupção para contar os pulsos
   pulseCount++;
 }
 
 void readRPM() {
   unsigned long currentTime = millis();
   
-  // Calcula as RPM a cada intervalo de tempo
   if (currentTime - lastTime >= interval) {
-    // Calcula as RPM
     float rpm = (float)pulseCount / (float(interval) / 60000.0); // Converte para minutos
-
-    // Exibe as RPM no Monitor Serial
-    if (Debug) {
-      Serial.print("RPM: ");
-      Serial.println(rpm);
-    }
     
-    valores[AddrNum - 1].add = 44;
-    valores[AddrNum - 1].valor = rpm;
+    if (rpm <= 1.0) {
+      valores[0].add = 0;
+      valores[0].valor = 10; 
+    }  
+    
+    valores[AddrNum - 2].add = 44;
+    valores[AddrNum - 2].valor = rpm;
 
     // Reinicia o contador de pulsos
     pulseCount = 0;
     lastTime = currentTime;
   }
 }
+//---------------------------------------------------------------------------------//
 
-
-//////////////////////////////// MODBUS MASTER /////////////////////////////////////////
-void   preTransmission(){
-  digitalWrite(MAX485_DE, 1);     // define direção de comunicação em alto (envio)
-}
-void   postTransmission(){
-  digitalWrite(MAX485_DE, 0);     // define direção de comunicação em baixo (recepção)
-}
-
-//-------------------------------------------------------------------------------------
-
-//////////////////////////////////// CONEXÃO //////////////////////////////////////////
-
-// Função responsavel por testar conexão com o computador via serial.
-void testaConexao(){
+//------------------------------------ CONEXÃO ------------------------------------//
+void testaConexao(){ // Função responsavel por testar conexão com o computador via serial.
     if (Serial.available() > 0) {
       String dadosRecebidos = Serial.readStringUntil('\n');
   
@@ -214,10 +203,9 @@ void testaConexao(){
    }
    delay(refreshConnection);
 }
+//--------------------------------------------------------------------------------//
 
-
-
-//// FUNÇÃO RESPONSÁVEL POR OBTER OS COMANDOS DO COMPUTADOR
+//------------------------------------ COMANDO -----------------------------------//
 int softwareReq(){
   if (Serial.available() > 0){
     String msg = Serial.readStringUntil('\n');
@@ -240,21 +228,25 @@ int softwareReq(){
     return 0;
   }
 }
+//---------------------------------------------------------------------------------//
 
-// FUNÇÃO RESPONSÁVEL PELA CONCATENAÇÃO E ENVIO DE DADOS AO COMPUTADOR //////
+//------------------------------------- ENVIO -------------------------------------//
 // Envia valores para o software
 void enviaValores(){
+  bool hasError = false;
   String mensagem="";
   String id="";
-  for (int i=0; i<AddrNum/2; i++){
+  for (int i=0; i < AddrNum; i++){
     if(i!= 0 && i!=AddrNum){
       mensagem += ";";
     }
     id = "";
-
     switch(valores[i].add) {
       case 0:
-        id = "Erro";
+        if (!hasError) {
+          id = "Erro";
+          hasError = true;
+        }
       break;
       case 2:
         id = "Vm";
@@ -306,20 +298,28 @@ void enviaValores(){
       break;
       case 44:
         id = "RPM";
-      break;  
+      break;
+      case 46:
+        id = "ExI";
+      break;
+      case 48:
+        id = "ExV";
+      break;
       case 66:
         id = "F";
       break;
     }
-    
     mensagem = mensagem + id + "=" + (String)valores[i].valor;
   }
   Serial.println(mensagem);
 }
+//---------------------------------------------------------------------------------//
 
-// Mostra valores com legenda no monitor serial
+//------------------------------------ MOSTRAR ------------------------------------//
 void mostraSerial() {
+  int ignorados = 0; 
   for (int add = 0; add < AddrNum/2; add++) {
+    bool valid = true;
     switch(valores[add].add) {
       case 0:
         Serial.print("Erro: ");
@@ -375,18 +375,34 @@ void mostraSerial() {
       case 44:
         Serial.print("RPM: ");
       break;
+      case 46:
+        Serial.print("ExI: ");
+      break;
+      case 48:
+        Serial.print("ExV: ");
+      break;
       case 66:
         Serial.print("F: ");
       break;
+      default:
+        valid = false;
+        ignorados++;
+      break;
     }
+    if (valid) {
     Serial.print("\t");
     Serial.print(valores[add].valor);
     Serial.print("\n");
+    }
   }
+  Serial.print("Ignorados: ");
+  Serial.print("\t");
+  Serial.print(ignorados);
+  Serial.print("\n");
 }
-//-------------------------------------------------------------------------
+//---------------------------------------------------------------------------------//
 
-////////////////// LED INDICADOR //////////////////////////////////////////
+//--------------------------------- LED INDICADOR ---------------------------------//
 // 'e' - erro; 's' - envio; 'l' - leitura; 't' - teste; 'd' - debug
 void ledState(char state) {
     switch(state) {
@@ -403,8 +419,9 @@ void led(int red, int green, int blue) {
   analogWrite(ledGreen, green);
   analogWrite(ledBlue, blue);
 }
+//----------------------------------------------------------------------------------//
 
-/////////////////// FUNÇÕES RESPONSÁVEIS PELA UNIFICAÇÃO //////////////////  
+//----------------------------------- UNIFICAÇÃO -----------------------------------//  
 int unificarInt(uint16_t a, uint16_t b){
   uint32_t combinado = ((uint32_t)a << 16) | b;
   int f;
@@ -418,3 +435,13 @@ float unificarFloat(uint16_t a, uint16_t b){
   memcpy(&f, &combinado, sizeof f);
   return f;
 }
+//---------------------------------------------------------------------------------//
+
+//--------------------------------- MODBUS MASTER ---------------------------------//
+void   preTransmission(){
+  digitalWrite(MAX485_DE, 1);     // define direção de comunicação em alto (envio)
+}
+void   postTransmission(){
+  digitalWrite(MAX485_DE, 0);     // define direção de comunicação em baixo (recepção)
+}
+//---------------------------------------------------------------------------------//
