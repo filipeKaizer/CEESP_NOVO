c0f5bc8a30ffa24b48cb50390ee9e4cdd7ce4c59
filipeKaizer <98619997+filipeKaizer@users.noreply.github.com>
2024-04-08 20:15:09 -0300

    Update Arduino.ino
    Comentarios melhorados;
    Sistema de busca de endereço;
    Leitura de corrente e tensão de excitatriz adicionados;
    Nova leitura no debug.

--- Arduino.ino
+++ Arduino.ino
@@ -1,26 +1,33 @@
 #include <ModbusMaster.h>
+#include <EEPROM.h>
+#include "Wire.h"
 
 //----------------------------------- DEFINIÇÕES -----------------------------------//
 ModbusMaster node;          // instantiate ModbusMaster object
 
-#define MAX485_DE      3      // Pino de sentido de transmissão
-#define ledRed         A0     // led - Red
-#define ledGreen       A1     // led - Green
-#define ledBlue        A2     // led - Blue
-#define sensorRPM      2      // Sensor de RPM, HIGH quando interrompido
-
-#define MMW2_ID        1      // ID do dispositivo medidor MMW02
-#define AddrNum        70     // Numero de endereços obtidos pelo MMW02
-#define refresh        60     // Tempo entre atualizações de dados do arduino (60 -> 60ms)
-#define refreshConnection 500 // Tempo de espera para o software enviar valores de teste
+#define MAX485_DE                               3      // Pino de sentido de transmissão
+#define ledRed                                  A0     // led - Red
+#define ledGreen                                A1     // led - Green
+#define ledBlue                                 A2     // led - Blue
+#define sensorRPM                               2      // Sensor de RPM, HIGH quando interrompido
+#define MMW2_ID                                 1      // ID do dispositivo medidor MMW02
+#define AddrNum                                 70     // Numero de endereços obtidos pelo MMW02
+#define refresh                                 60     // Tempo entre atualizações de dados do arduino (60 -> 60ms)
+#define refreshConnection                       500 // Tempo de espera para o software enviar valores de teste
+#define Conf_INA                                0
+#define Conf_eeprom_INA                         5
+#define INA260_ADDRESS                          (0x41)    // 1000000 (A0+A1=GND) ou (0x41) 1000001 (A0=VCC , A1=GND)
+#define INA260_REG_CONFIG                       (0x00)
+#define INA260_CONFIG_AVGRANGE_4                (0x0200)  // Average mode 4
+#define INA260_CONFIG_BVOLTAGETIME_8244US       (0x01C0)  // 8.244ms
+#define INA260_CONFIG_SCURRENTTIME_8244US       (0x0038)  // 8.244ms
+#define INA260_CONFIG_MODE_SANDBVOLT_CONTINUOUS (0x0007)  //Shunt Current and Bus Voltage,Continuous
 //--------------------------------------------------------------------------------//
 
 //----------------------------------- VARIAVEIS ----------------------------------//
-bool Debug = false;         // Flag que habilita a função debug. 
-
-// Variáveis para o cálculo das RPM
-volatile unsigned long pulseCount = 0;
-volatile unsigned long lastTime = 0;
+bool Debug = false;                     // Flag que habilita a função debug. 
+volatile unsigned long pulseCount = 0;  // Contagem de pulsos do RPM
+volatile unsigned long lastTime = 0;    // Ultima leitura
 volatile unsigned long interval = 1000; // Intervalo de leitura em milissegundos
 
 //// Especifica o formato de cada leitura
@@ -28,7 +35,6 @@ struct Valor {
   int add;
   float valor;
 };
-
 //// Especifica um vetor da struct valor
 Valor valores[AddrNum];
 //--------------------------------------------------------------------------------//
@@ -38,7 +44,7 @@ Valor valores[AddrNum];
   snd      -> envio de dados concatenados 
   serial   -> Envio de dados como lista
   test     -> Função de compatibilidade
-  debug    -> Ativa a função de debug, mostrando leitura em tempo real
+  debug    -> Ativa a função de debug, mostrando a leitura em tempo real
 */
 //--------------------------------------------------------------------------------//
 
@@ -50,16 +56,17 @@ void setup(){
 
   pinMode(sensorRPM, INPUT);                    // RPM 
   attachInterrupt(digitalPinToInterrupt(sensorRPM), countPulse, FALLING); // Ativa a interrupção no pino do sensor
-   
   pinMode(MAX485_DE, OUTPUT);                   // Pino de controle do MAX485
-  digitalWrite(MAX485_DE, 0);                   // Sentido do MAX485     
+  digitalWrite(MAX485_DE, 0);                   // Sentido do MAX485    
+   
   Serial1.begin(9600, SERIAL_8N1);              // Serial para comunicação com o MMW02
   Serial.begin(9600);                           // Monitor serial para visualização de dados.
+  Wire.begin();
   
   node.begin(MMW2_ID, Serial1);                 // Inicia o objeto node para comunicação com o MMW02
   node.preTransmission(preTransmission);        // Modbus
   node.postTransmission(postTransmission);      // Modbus
-}
+ }
 // --------------------------------------------------------------------------------//
 
 //-------------------------------------- LOOP -------------------------------------//
@@ -82,17 +89,24 @@ void loop() {
     default:
      ledState('l');
      Leitura();
+     excitatriz();
      readRPM();
      if (Debug)
        debug();
     break;    
   }
   delay(refresh);
-}
+ }
 // ---------------------------------------------------------------------------------//
 
 //----------------------------------- DEBUG MODE -----------------------------------//
 void debug() {
+    // Limpar tela
+    Serial.write(27);
+    Serial.print("[2J");
+    Serial.write(27);
+    Serial.print("[H");
+
     // Dados coletados do modbus 
     Serial.print("\n\tMULTIMEDIDOR\t\n");
     if (valores[0].valor != 0) {
@@ -112,13 +126,27 @@ void debug() {
       Serial.print("RPM)\n");
     } else {
       Serial.print("RPM: \t");
-      Serial.print(valores[44].valor);
+      Serial.print(valores[findIndex(44)].valor);
       Serial.print("RPM\n");
     }
-}
-//---------------------------------------------------------------------------------//
 
-//------------------------------------ LEITURA ------------------------------------//
+    // Excitatriz
+    Serial.print("\n\tEXCITATRIZ\t\n");
+    if (valores[findIndex(46)].valor == 0 && valores[findIndex(48)].valor == 0) {
+      Serial.print("Erro: \t");
+      Serial.print("Falha na leitura da excitatriz.\n");
+    } else {
+      Serial.print("I: \t");
+      Serial.print(valores[findIndex(46)].valor);
+      Serial.print("A\n");
+      Serial.print("V: \t");
+      Serial.print(valores[findIndex(48)].valor);
+      Serial.print("V\n");
+    }
+ }
+//----------------------------------------------------------------------------------//
+
+//------------------------------------ LEITURA -------------------------------------//
 void Leitura(){
   uint16_t result, result2;
   float floatValue;
@@ -156,13 +184,13 @@ void Leitura(){
     
     ledState('e');
   }
-}
-//---------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//-------------------------------------- RPM --------------------------------------//
+//-------------------------------------- RPM ---------------------------------------//
 void countPulse() { // Função de interrupção para contar os pulsos
   pulseCount++;
-}
+ }
 
 void readRPM() {
   unsigned long currentTime = millis();
@@ -182,10 +210,10 @@ void readRPM() {
     pulseCount = 0;
     lastTime = currentTime;
   }
-}
-//---------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//------------------------------------ CONEXÃO ------------------------------------//
+//------------------------------------ CONEXÃO -------------------------------------//
 void testaConexao(){ // Função responsavel por testar conexão com o computador via serial.
     if (Serial.available() > 0) {
       String dadosRecebidos = Serial.readStringUntil('\n');
@@ -196,16 +224,16 @@ void testaConexao(){ // Função responsavel por testar conexão com o computado
        int valor1 = dadosRecebidos.substring(0, posicaoVirgula).toInt();
        int valor2 = dadosRecebidos.substring(posicaoVirgula + 1).toInt();
   
-       int result = (valor1 % valor2) * (valor1+valor2);
+       int result = (valor1 % valor2) * (valor1 + valor2);
   
        Serial.println(result);
       }
    }
    delay(refreshConnection);
-}
-//--------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//------------------------------------ COMANDO -----------------------------------//
+//------------------------------------ COMANDO -------------------------------------//
 int softwareReq(){
   if (Serial.available() > 0){
     String msg = Serial.readStringUntil('\n');
@@ -227,11 +255,10 @@ int softwareReq(){
   } else {
     return 0;
   }
-}
-//---------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//------------------------------------- ENVIO -------------------------------------//
-// Envia valores para o software
+//------------------------------------- ENVIO --------------------------------------//
 void enviaValores(){
   bool hasError = false;
   String mensagem="";
@@ -312,10 +339,10 @@ void enviaValores(){
     mensagem = mensagem + id + "=" + (String)valores[i].valor;
   }
   Serial.println(mensagem);
-}
-//---------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//------------------------------------ MOSTRAR ------------------------------------//
+//------------------------------------ MOSTRAR -------------------------------------//
 void mostraSerial() {
   int ignorados = 0; 
   for (int add = 0; add < AddrNum/2; add++) {
@@ -399,49 +426,154 @@ void mostraSerial() {
   Serial.print("\t");
   Serial.print(ignorados);
   Serial.print("\n");
-}
-//---------------------------------------------------------------------------------//
+ }
+//----------------------------------------------------------------------------------//
 
-//--------------------------------- LED INDICADOR ---------------------------------//
+//-------------------------------- EXCITATRIZ (INA) --------------------------------//
+void excitatriz() {
+  valores[AddrNum - 4].add = 46;
+  valores[AddrNum - 4].valor = getcurrent(INA260_ADDRESS);
+
+  valores[AddrNum - 6].add = 48;
+  valores[AddrNum - 6].valor = 1000 * getvoltage(INA260_ADDRESS);
+ }
+void Ina_begin(uint8_t addr){
+  uint16_t conf;
+  
+  if (EEPROM.read(Conf_eeprom_INA) == 0x02){
+    conf = word(EEPROM.read(Conf_INA), EEPROM.read(Conf_INA+1));
+  }
+  
+  else{
+    conf =   INA260_CONFIG_AVGRANGE_4 |
+             INA260_CONFIG_BVOLTAGETIME_8244US |
+             INA260_CONFIG_SCURRENTTIME_8244US |
+             INA260_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
+    }
+  
+  Wire.beginTransmission(addr);
+  #if ARDUINO >= 100
+    Wire.write(0x00);                       // Register
+    Wire.write((conf >> 8) & 0xFF);       // Upper 8-bits
+    Wire.write(conf & 0xFF);              // Lower 8-bits
+  #else
+    Wire.send(0x00);                        // Register
+    Wire.send(conf >> 8);                 // Upper 8-bits
+    Wire.send(conf & 0xFF);               // Lower 8-bits
+  #endif
+  Wire.endTransmission();
+ }
+float getvoltage(uint8_t addr){
+  int16_t valor;
+  float   tensao;
+  
+  Wire.beginTransmission(addr);
+  #if ARDUINO >= 100
+    Wire.write(0x02);                       // Register  0x02 registrador tensão
+  #else
+    Wire.send(0x02);                        // Register  0x02 registrador tensão
+  #endif
+  Wire.endTransmission();
+  
+  delay(1); // Max 12-bit conversion time is 586us per sample
+
+  Wire.requestFrom(addr, (uint8_t)2);  
+  #if ARDUINO >= 100
+    // Shift values to create properly formed integer
+    valor = ((Wire.read() << 8) | Wire.read());
+  #else
+    // Shift values to create properly formed integer
+    valor = ((Wire.receive() << 8) | Wire.receive());
+  #endif
+
+  tensao = valor*0.00125/0.909;
+  if(tensao<0)tensao=0;
+  delay(10);
+  return tensao;
+  }
+
+float getcurrent(uint8_t addr){
+  
+  int16_t valor;
+  float corrente;
+  Wire.endTransmission();
+  Wire.beginTransmission(addr);
+  #if ARDUINO >= 100
+    Wire.write(0x01);                       // Register  0x02 registrador tensão
+  #else
+    Wire.send(0x01);                        // Register  0x02 registrador tensão
+  #endif
+  Wire.endTransmission();
+  
+  delay(1); // Max 12-bit conversion time is 586us per sample
+
+  Wire.requestFrom(addr, (uint8_t)2);  
+  #if ARDUINO >= 100
+    // Shift values to create properly formed integer
+    valor = ((Wire.read() << 8) | Wire.read());
+  #else
+    // Shift values to create properly formed integer
+    valor = ((Wire.receive() << 8) | Wire.receive());
+  #endif
+
+  corrente = valor* 1.25;
+  if(corrente<0) corrente=0;
+  delay(10);
+  return corrente;
+  }
+//----------------------------------------------------------------------------------//
+
+//--------------------------------- LED INDICADOR ----------------------------------//
 // 'e' - erro; 's' - envio; 'l' - leitura; 't' - teste; 'd' - debug
 void ledState(char state) {
     switch(state) {
-      case 'e': led(255, 0, 0); break;
-      case 's': led(0, 255, 0); break;
-      case 'l': led(0, 0, 255); break;
-      case 't': led(255, 255, 0); break;
-      case 'd': led(255, 0, 255); break;
+      case 'e': led(255, 0, 0); break;   // Erro    -> Vermelho
+      case 's': led(0, 255, 0); break;   // Envio   -> Verde
+      case 'l': led(0, 0, 255); break;   // Leitura -> Azul
+      case 't': led(255, 255, 0); break; // Teste   -> Amarelo
+      case 'd': led(255, 0, 255); break; // Debug   -> Roxo
     }
-}
+ }
 
 void led(int red, int green, int blue) {
   analogWrite(ledRed, red);
   analogWrite(ledGreen, green);
   analogWrite(ledBlue, blue);
-}
+ }
 //----------------------------------------------------------------------------------//
 
 //----------------------------------- UNIFICAÇÃO -----------------------------------//  
 int unificarInt(uint16_t a, uint16_t b){
-  uint32_t combinado = ((uint32_t)a << 16) | b;
-  int f;
+  uint32_t combinado = ((uint32_t)a << 16) | b; // Deslocamento de 16 bits de A e 
+  int f;                                        // concatenação de 16 bits de B.
   memcpy(&f, &combinado, sizeof f);
   return f;
-}
+ }
 
 float unificarFloat(uint16_t a, uint16_t b){            
   uint32_t combinado = ((uint32_t)a << 16) | b;
   float f;
   memcpy(&f, &combinado, sizeof f);
   return f;
-}
+ }
+//----------------------------------------------------------------------------------//
+
+//---------------------------------- BUSCA INDEX -----------------------------------//
+int findIndex(int id) {
+  int i = 0;
+  for (; i < AddrNum; i++) {
+    if (valores[i].add == id) 
+      return i;
+  }
+  return 0;
+ }
 //---------------------------------------------------------------------------------//
 
 //--------------------------------- MODBUS MASTER ---------------------------------//
 void   preTransmission(){
   digitalWrite(MAX485_DE, 1);     // define direção de comunicação em alto (envio)
-}
+ }
 void   postTransmission(){
   digitalWrite(MAX485_DE, 0);     // define direção de comunicação em baixo (recepção)
-}
+ }
 //---------------------------------------------------------------------------------//
